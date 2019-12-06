4fccb14461f9f10aa19391f47714cba86d115408
MccarthyRoman <mac@Romans-MacBook-Pro.local>
2019-12-07 01:36:26 +0200

    lamp in progress and it works with js

--- lamp.js
+++ lamp.js
@@ -1,1 +1,13 @@
-js code
+console.log('Hello')
+
+
+function myFunction() {
+	var lbulb = document.querySelector("#bulb")
+	lbulb.setAttribute('style', 
+	'background: yellow;box-shadow:0 0 100px yellow;');
+}
+function myFunction2() {
+	var lbulb = document.querySelector("#bulb")
+	lbulb.setAttribute('style', 
+	'background: #C8C8C8;');
+}
